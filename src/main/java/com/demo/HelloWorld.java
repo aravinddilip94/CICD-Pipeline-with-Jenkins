2d84111fd4adb1377248ba1cc36932f0a9c5d2a5
aravinddilip94 <aravinddilip94>
2025-08-16 19:42:23 +0530

    code modified

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellooooo, Jenkins Pipeline!");
+        System.out.println("Hello!!!, Jenkins Pipeline!");
     }
 }
